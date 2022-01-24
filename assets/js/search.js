2866717805b3934991b4afd1a393731342d18798
ernavdeepkr <50796185+ernavdeepkr@users.noreply.github.com>
2022-01-24 12:30:56 -0500

    Changed the date string to UTC

--- assets/js/search.js
+++ assets/js/search.js
@@ -154,7 +154,7 @@ function executeSearch(term) {
 
     for (let item in results.slice(0, 5)) { // only show first 5 results
       const title = '<div class="text-2xl mb-2 font-bold">' + results[item].item.title + '</div>';
-      const date = results[item].item.date ? '<div><em class="">' + new Date(results[item].item.date).toDateString() + '</em></div>' : '';
+      const date = results[item].item.date ? '<div><em class="">' + new Date(results[item].item.date).toUTCString().substring(0,16) + '</em></div>' : '';
       const contents = '<div>' + results[item].item.contents + '</div>';
 
       searchitems = searchitems + '<li><a class="block mb-2 px-4 py-2 rounded pb-2 border-b border-gray-200 dark:border-gray-600 focus:bg-gray-100 dark:focus:bg-gray-700 focus:outline-none" href="' + results[item].item.permalink + '" tabindex="0">' + title + date + contents + '</a></li>';
